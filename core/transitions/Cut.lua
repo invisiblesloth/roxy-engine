06592753745bdedcc256ba81b95cce0bdc5d430a
Jason Kisner <jasonkisner@gmail.com>
2024-09-13 16:16:03 -0400

    Refresh .gitignore rules

--- core/transitions/Cut.lua
+++
@@ -1,9 +0,0 @@
--- Cut is a transition effect that instantly switches from the 
--- current scene to the new scene without any fade or delay.
-
-class("Cut").extends(RoxyCutTransition)
-local transition = Cut
-
-function transition:init(duration, holdTime, properties)
-	transition.super.init(self, duration, holdTime, properties)
-end
