6d3569735e4a35869d65ad7b779ac8ba6491cdb6
Jason Kisner <jasonkisner@gmail.com>
2025-06-18 16:49:52 -0400

    refactor(Core): move aliases below global state and add r alias
    Ensure roxy table is defined before aliasing. Introduce `r` as an alias 
    for `roxy` and update usages throughout for improved style and 
    performance.

--- roxy.lua
+++ roxy.lua
@@ -24,11 +24,6 @@ import "CoreLibs/ui/gridview"
 local DEFAULT_FPS_X <const> = 385 --#DEBUG
 local DEFAULT_FPS_Y <const> = 228 --#DEBUG
 
--- ! Aliases
-
-local pd            <const> = playdate
-local getDeltaTime  <const> = roxy.getDeltaTime
-
 -- ! Global State
 
 -- Create global Roxy table if it does not already exist
@@ -38,6 +33,15 @@ roxy = roxy or {}
 
 local engineInitialized = false
 
+-- ! Aliases
+
+-- SDK & Module
+local pd  <const> = playdate
+local r   <const> = roxy
+
+--Performance
+local getDeltaTime  <const> = r.getDeltaTime
+
 -- ----------------------------------------
 -- ! Public API
 -- ----------------------------------------
@@ -45,17 +49,17 @@ local engineInitialized = false
 -- ! Core Engine Functions
 
 -- Initializes the engine and transitions to the first scene
-function roxy.new()
+function r.new()
 
 end
 
 -- ! System Callbacks
 
-function roxy.gameWillPause()
+function r.gameWillPause()
 
 end
 
-function roxy.gameWillResume()
+function r.gameWillResume()
 
 end
 
@@ -67,6 +71,6 @@ end
 
 function pd.update()
   local dt = getDeltaTime()
-  roxy.deltaTime = dt
+  r.deltaTime = dt
   print(dt)
 end
