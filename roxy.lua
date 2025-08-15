56dde1f4a36f69eae3bc965aa79bfbe29a3f39c5
Jason Kisner <jasonkisner@gmail.com>
2025-08-15 16:31:52 -0400

    feat(core): add import for RoxyStagTilemap to main roxy module
    Ensure staggered-y isometric tilemap support is available by default 
    through the core roxy import list.

--- roxy.lua
+++ roxy.lua
@@ -53,6 +53,7 @@ import "libraries/roxy/core/animations/RoxyAnimation"
 import "libraries/roxy/core/tilemaps/RoxyTilemap"
 import "libraries/roxy/core/tilemaps/RoxyOrthoTilemap"
 import "libraries/roxy/core/tilemaps/RoxyIsoTilemap"
+import "libraries/roxy/core/tilemaps/RoxyStagTilemap"
 import "libraries/roxy/core/scenes/RoxyScene"
 
 -- Create global Roxy table if it does not already exist
